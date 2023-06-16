a14e8edd6b3977a6edd520b3ff6766e50b5c801e
marwan <marwan.h1994@gmail.com>
2023-06-16 23:04:03 +0200

    add fun to exe2 to avoid repeat self

--- Week1/homework/EXE-2/worldSql.js
+++ Week1/homework/EXE-2/worldSql.js
@@ -8,150 +8,106 @@ const connection = mysql.createConnection({
     password: "hyfpassword",
 })
 
-connection.connect((err) => {
-    if (err) throw err
-    console.log("connected to database")
-})
-
-connection.query("CREATE DATABASE IF NOT EXISTS world", (err) => {
-    if (err) throw err
-    console.log("World database has been created")
-})
-
-connection.query("USE world", (err) => {
-    if (err) throw err
-})
-
-const sqlFilePath = path.join(__dirname, "world.sql")
-const sql = fs.readFileSync(sqlFilePath, "utf8")
-
-const sqlStatements = sql.split(";")
-
-sqlStatements.forEach((statement) => {
-    connection.query(statement, (err) => {
-        if (err) throw err
+function executeQuery(query) {
+    return new Promise((resolve, reject) => {
+        connection.query(query, (err, results) => {
+            if (err) reject(err)
+            resolve(results)
+        })
     })
-})
-console.log("All SQL statements executed successfully")
+}
 
-connection.query(
-    "SELECT Name FROM country WHERE Population > 8000000",
-    (err, results) => {
-        if (err) throw err
-        console.log("The countries with Population greater than 8 million")
+async function executeAndPrintQuery(query, description) {
+    try {
+        const results = await executeQuery(query)
+        console.log(description)
         results.forEach((row) => {
-            console.log(row.Name)
+            console.log(row.Name || row.Population)
         })
         console.log("___________________________________________________")
+    } catch (err) {
+        console.error("An error occurred while executing the query:", err)
     }
-)
+}
 
-connection.query(
-    'SELECT Name FROM country WHERE Name LIKE "%land%"',
-    (err, results) => {
-        if (err) throw err
-        console.log("Countries that have land word in their names")
-        results.forEach((row) => {
-            console.log(row.Name)
-        })
-        console.log("___________________________________________________")
-    }
-)
+async function runQueries() {
+    try {
+        connection.connect()
+        console.log("Connected to the database")
 
-connection.query(
-    "SELECT Name FROM city WHERE Population BETWEEN  500000 AND 1000000",
-    (err, results) => {
-        if (err) throw err
-        console.log("Cities with population between 500,000 and 1 million")
-        results.forEach((row) => {
-            console.log(row.Name)
-        })
-        console.log("___________________________________________________")
-    }
-)
+        await executeQuery("CREATE DATABASE IF NOT EXISTS world")
+        console.log("World database has been created")
 
-connection.query(
-    "SELECT Name FROM country WHERE Continent = 'Europe'",
-    (err, results) => {
-        if (err) throw err
-        console.log("Countries in the continent 'Europe':")
-        results.forEach((row) => {
-            console.log(row.Name)
-        })
-        console.log("___________________________________________________")
-    }
-)
+        await executeQuery("USE world")
 
-connection.query(
-    "SELECT Name FROM country ORDER BY SurfaceArea DESC",
-    (err, results) => {
-        if (err) throw err
-        console.log("Countries in descending order of surface area:")
-        results.forEach((row) => {
-            console.log(row.Name)
-        })
-        console.log("___________________________________________________")
-    }
-)
+        const sqlFilePath = path.join(__dirname, "world.sql")
+        const sql = fs.readFileSync(sqlFilePath, "utf8")
+        const sqlStatements = sql.split(";")
 
-connection.query(
-    "SELECT Name FROM city  WHERE CountryCode = 'NLD'",
-    (err, results) => {
-        if (err) throw err
-        console.log("Cities in the Netherlands:")
-        results.forEach((row) => {
-            console.log(row.Name)
-        })
-        console.log("___________________________________________________")
-    }
-)
+        await sqlStatements.reduce(
+            (promise, query) => promise.then(() => executeQuery(query)),
+            Promise.resolve()
+        )
 
-connection.query(
-    "SELECT Population FROM city WHERE Name ='Rotterdam' ",
-    (err, results) => {
-        if (err) throw err
-        console.log("Population of Rotterdam:")
-        results.forEach((row) => {
-            console.log(row.Population)
-        })
-        console.log("___________________________________________________")
-    }
-)
+        console.log("All SQL statements executed successfully")
 
-connection.query(
-    "SELECT Name FROM country ORDER BY SurfaceArea DESC LIMIT 10",
-    (err, results) => {
-        if (err) throw err
-        console.log("Top 10 countries by Surface Area:")
-        results.forEach((row) => {
-            console.log(row.Name)
-        })
-        console.log("___________________________________________________")
-    }
-)
+        await executeAndPrintQuery(
+            "SELECT Name FROM country WHERE Population > 8000000",
+            "The countries with Population greater than 8 million"
+        )
 
-connection.query(
-    "SELECT Name FROM city ORDER BY Population DESC LIMIT 10",
-    (err, results) => {
-        if (err) throw err
-        console.log("Top 10 most populated cities:")
-        results.forEach((row) => {
-            console.log(row.Name)
-        })
-        console.log("___________________________________________________")
-    }
-)
+        await executeAndPrintQuery(
+            'SELECT Name FROM country WHERE Name LIKE "%land%"',
+            "Countries that have 'land' in their names"
+        )
 
-connection.query(
-    "SELECT SUM(Population) AS WorldPopulation FROM country",
-    (err, results) => {
-        if (err) throw err
+        await executeAndPrintQuery(
+            "SELECT Name FROM city WHERE Population BETWEEN 500000 AND 1000000",
+            "Cities with population between 500,000 and 1 million"
+        )
+
+        await executeAndPrintQuery(
+            "SELECT Name FROM country WHERE Continent = 'Europe'",
+            "Countries in the continent 'Europe':"
+        )
+
+        await executeAndPrintQuery(
+            "SELECT Name FROM country ORDER BY SurfaceArea DESC",
+            "Countries in descending order of surface area:"
+        )
+
+        await executeAndPrintQuery(
+            "SELECT Name FROM city WHERE CountryCode = 'NLD'",
+            "Cities in the Netherlands:"
+        )
+
+        await executeAndPrintQuery(
+            "SELECT Population FROM city WHERE Name ='Rotterdam'",
+            "Population of Rotterdam:"
+        )
+
+        await executeAndPrintQuery(
+            "SELECT Name FROM country ORDER BY SurfaceArea DESC LIMIT 10",
+            "Top 10 countries by Surface Area:"
+        )
+
+        await executeAndPrintQuery(
+            "SELECT Name FROM city ORDER BY Population DESC LIMIT 10",
+            "Top 10 most populated cities:"
+        )
+
+        const results10 = await executeQuery(
+            "SELECT SUM(Population) AS WorldPopulation FROM country"
+        )
         console.log("Population number of the world:")
-        console.log(results[0].WorldPopulation)
+        console.log(results10[0].WorldPopulation)
         console.log("___________________________________________________")
+
+        connection.end()
+        console.log("Disconnected from MySQL server")
+    } catch (err) {
+        console.error("An error occurred:", err)
     }
-)
-connection.end((err) => {
-    if (err) throw err
-    console.log("Disconnected from MySQL server")
-})
+}
+
+runQueries()
